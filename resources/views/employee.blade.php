0d2ee613f1f86e48e6089972ac939cb74d127672
lavakush7800 <lavakushgiri@gmail.com>
2021-07-20 18:30:35 +0530

    auth

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>OnSale</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
+@extends('layouts.app')
 
+@section('content')
 <div class="container">
   <center><h2>Employee Details</h2></center>
   <form action="/action_page.php">
@@ -30,6 +20,4 @@
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
 </div>
-
-</body>
-</html>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                    <li><a href="employee">Employee</a></li>
 
                     </ul>
 
